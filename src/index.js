354dacf7caaebe9361850346e5a0f779317ef58a
Mahathir <mirmahathir1@gmail.com>
2020-09-10 11:58:01 +0600

    Enabled cors

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const cors = require('cors');
 
 const app = express()
 const port = process.env.PORT
 
-
+app.use(cors());
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
