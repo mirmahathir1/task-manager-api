12704b536a8b8c7acc86bc0ec0203df27fe3d68a
Mahathir <mirmahathir1@gmail.com>
2020-09-11 17:25:57 +0600

    quiz deletion enabled

--- src/routers/quiz.js
+++ src/routers/quiz.js
@@ -3,8 +3,8 @@ const Quiz = require('../models/quiz')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 
-router.post('/quizzes', auth,async (req, res) => {
-    const quizData = {...req.body,owner:req.user._id};
+router.post('/quizzes', auth, async (req, res) => {
+    const quizData = {...req.body, owner: req.user._id};
     quizData.startTime = new Date(quizData.startTime)
 
     const quiz = new Quiz(quizData)
@@ -16,6 +16,21 @@ router.post('/quizzes', auth,async (req, res) => {
         console.log(e);
         res.status(400).send(e)
     }
+})
+
+router.delete('/quizzes/:id', auth, async (req, res) => {
+    try {
+        const quiz = await Quiz.findOneAndDelete({_id: req.params.id, owner: req.user._id})
+
+        if (!quiz) {
+            res.status(404).send()
+        }
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+
 })
 
 module.exports = router
